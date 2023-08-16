5cf6347acd29840acf94e59651a56b73fff8ad50
maryamsakhaei <maryamsakhaei66@yahoo.com>
2023-08-16 10:52:31 +0200

    login

--- admin/logout.php
+++ admin/logout.php
@@ -2,8 +2,10 @@
 session_start();
 
 if (isset($_GET['logout'])) {
+   unset($_SESSION['User']);
    unset($_SESSION['Adm']);
+   unset($_SESSION['Agency']);
    session_unset();
    session_destroy();
-   header("Location: login.php");
+   header("refresh: 2; url = ../user/login.php");
 }
