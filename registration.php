909d5dee9a9c0a454b08e002adde25169e9c6e18
elenimingko <elenimingo889@yahoo.com>
2023-08-16 09:13:11 +0200

    n

--- registration.php
+++ registration.php
@@ -19,8 +19,8 @@ if (isset($_SESSION["adm"])) {
 }
 $error = false;
 
-$fname = $lname = $email = $date_of_birth = $email = "";
-$fnameError = $lnameError = $dateError = $emailError = $passError = "";
+$fname = $lname = $email =$address  =$phone=$space=$experienced =$password= $date_of_birth = "";
+$fnameError = $lnameError = $dateError= $addressError =$phoneError= $emailError = $passError = "";
 
 function cleanInput($param)
 {
@@ -32,12 +32,16 @@ function cleanInput($param)
 }
 
 if (isset($_POST["sign-up"])) {
-    $fname = cleanInput($_POST["fname"]);
-    $lname = cleanInput($_POST["lname"]);
+    $fname = cleanInput($_POST["firstName"]);
+    $lname = cleanInput($_POST["lastName"]);
     $email = cleanInput($_POST["email"]);
+    $address = cleanInput($_POST["address"]);
+    $phone = cleanInput($_POST["phone"]);
+    $space = cleanInput($_POST["space"]);
+    $experienced = cleanInput($_POST["experienced"]);
     $password = $_POST["password"];
-    $date_of_birth = cleanInput($_POST["date_of_birth"]);
-    $picture = upload($_FILES["picture"]);
+    $date_of_birth = cleanInput($_POST["birthdate"]);
+    $picture = upload($_FILES["image"]);
 
     if (empty($fname)) {
         $error = true;
@@ -63,7 +67,7 @@ if (isset($_POST["sign-up"])) {
 
     if (empty($date_of_birth)) {
         $error = true;
-        $dateError = "date of birth can't be empty!";
+        $dateError = "Date of birth can't be empty!";
     }
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -77,7 +81,36 @@ if (isset($_POST["sign-up"])) {
             $emailError = "Provided Email is already in use";
         }
     }
-
+    if (empty($address)) {
+        $error = true;
+        $$addressError = "Please, enter your Address";
+    } elseif (strlen($address) < 3) {
+        $error = true;
+        $addressError = "Address must have at least 3 characters.";
+    } elseif (!preg_match("/^[a-zA-Z\s]+$/", $address)) {
+        $error = true;
+        $addressError = "Address must contain only letters and spaces.";
+    }
+    if (empty($phone)) {
+        $error = true;
+        $phoneError = "Please, enter your Phone Number";
+    } elseif (strlen($phone) < 3) {
+        $error = true;
+        $phoneError = "Please give a valid phone number";
+    } elseif (!preg_match("/^[a-zA-Z\s]+$/",$phone)) {
+        $error = true;
+        $phoneError = "Phone number most contain only numbers";
+    }
+    if (empty($space)) {
+        $error = true;
+        $spaceError = "Please, enter your Space";
+    } elseif (strlen($space) < 2) {
+        $error = true;
+        $spaceError = "Address must have at least 2 characters.";
+    } elseif (!preg_match("/^[a-zA-Z\s]+$/",$space)) {
+        $error = true;
+        $spaceError = "Space";
+    }
     if (empty($password)) {
         $error = true;
         $passError = "Password can't be empty!";
@@ -88,7 +121,7 @@ if (isset($_POST["sign-up"])) {
 
     if (!$error) {
         $password = hash("sha256", $password);
-        $sql = "INSERT INTO `users`( `first_name`, `last_name`, `password`, `date_of_birth`, `email`, `picture`) VALUES ('$fname','$lname','$password','$date_of_birth','$email','$picture[0]') ";
+        $sql = "INSERT INTO `users`(`firstName`, `lastName`,`address`, `image`, `birthdate`, `phone`, `email`, `password`, `space`, `experienced`) VALUES ('$fname','$lname','$address','$image','$birthdate','$phone','$email','$password','$space','$experienced') ";
 
         if (mysqli_query($connect, $sql)) {
             echo   "<div class='alert alert-success'>
@@ -140,29 +173,35 @@ if (isset($_POST["sign-up"])) {
             <h1 class="text-center">Sign Up </h1>
             <form method="post" autocomplete="off" enctype="multipart/form-data">
                 <div class="mb-3 mt-3" >
-                    <label for="fname" class="form-label">First name </label>
-                    <input type="text" class="form-control" id="fname" name="fname" placeholder="First name" value="<?=$fname?>" >
+                    <label for="firstName" class="form-label">First name </label>
+                    <input type="text" class="form-control" id="fname" name="firstName" placeholder="First name" value="<?=$fname?>" >
                     <span class="text-danger"><?=$fnameError?></span>
                 </div>
                 <div class="mb-3">
-                    <label for="lname" class="form-label">Last name </label>
-                    <input type="text" class="form-control" id="lname" name="lname" placeholder="Last name"value="<?=$lname?>">
+                    <label for="lastName" class="form-label">Last name </label>
+                    <input type="text" class="form-control" id="lname" name="lastName" placeholder="Last name"value="<?=$lname?>">
                     <span class="text-danger"><?=$lnameError?></span>
                 </div>
                 <div class="mb-3">
-                    <label for="date" class="form-label">Date of birth</label>
-                    <input type="date" class= "form-control" id="date" name="date_of_birth" value="<?=$date_of_birth?>">
+                    <label for="birthdate" class="form-label">Date of birth</label>
+                    <input type="date" class= "form-control" id="date" name="birthdate" value="<?=$date_of_birth?>">
                     <span class="text-danger"><?=$dateError?></span>
                 </div>
                 <div class="mb-3">
-                    <label for="picture" class="form-label">Profile picture </label>
-                    <input type="file" class="form-control" id="picture" name="picture">
+                    <label for="image" class="form-label">Profile picture </label>
+                    <input type="file" class="form-control" id="picture" name="image">
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address </label>
                     <input type="email" class="form-control" id="email" name="email" placeholder="Email address" value="<?=$email?>">
                     <span class="text-danger"><?=$emailError?></span>
                 </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Address" value="<?=$address?>">
+                    <span class="text-danger"><?=$addressError?></span>
+                </div>
+             
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control" id="password" name="password">
