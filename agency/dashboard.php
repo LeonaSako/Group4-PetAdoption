0e1acdf106ae1ace2859e1c68e62d1c5176a104c
LivadeHe <lisavandenheuvel@live.nl>
2023-08-16 14:08:36 +0200

    WIP Dashboard + Table Agency

--- agency/dashboard.php
+++ agency/dashboard.php
@@ -1,29 +1,41 @@
 <?php
-require_once "../utils/crud.php";
-require_once "../utils/formUtils.php";
-
 session_start();
-preventUser();
-preventAdmin();
 
-# This is a script that handles the agency's dashboard. Only the agency is allowed here.
+require_once "../utils/crud.php";
+require_once "../pet/viewAll.php";
 
-# To bring all pets listed by the agency, use $results = $crud->selectPets("fk_users_id = {$_SESSION["Agency"]}");
+$crud = new CRUD();
+$agencyId = $_SESSION["Agency"];
+
+
+
+$result = $crud->selectPets("`fk_users_id` = $agencyId"); 
+
+$layout = viewPets($result);
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Title</title>
 </head>
 
 <body>
     <?php include '../components/navbar.php'; ?>
-    <!-- Add layout -->
+    <div class="container">
+        <div id="layout" class="row">
+            <?= $layout ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+   
 </body>
 
 </html>
