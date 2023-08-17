31d134047156349515b306707f7cf7ed9ea1156c
elenimingko <elenimingo889@yahoo.com>
2023-08-17 09:19:22 +0200

    new

--- adoptions/new.php
+++ adoptions/new.php
@@ -31,6 +31,7 @@ $userID = $_SESSION["User"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
@@ -38,6 +39,7 @@ $userID = $_SESSION["User"];
     <?php include '../components/navbar.php'; ?>
 
     <!-- Add layout -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
 </body>
 
