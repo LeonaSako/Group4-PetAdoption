26556e35f6fa109d09f6fa3f00eae35005ec7798
maryamsakhaei <maryamsakhaei66@yahoo.com>
2023-08-18 20:05:45 +0200

    wip

--- adoptions/new.php
+++ adoptions/new.php
@@ -34,7 +34,6 @@ if (isset($_POST['adoption-submit'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -42,7 +41,6 @@ if (isset($_POST['adoption-submit'])) {
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-
 <body>
     <?php include '../components/navbar.php'; ?>
     <?php
@@ -144,12 +142,7 @@ if (isset($_POST['adoption-submit'])) {
                         ";
         }
     }
-
-
-
-
     ?>
-
 </body>
 
 </html>
