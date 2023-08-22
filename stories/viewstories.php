b2f58f1647daae2aaa993468030283aad2bdd277
maryamsakhaei <maryamsakhaei66@yahoo.com>
2023-08-22 09:36:16 +0200

    wip

--- stories/viewstories.php
+++ stories/viewstories.php
@@ -4,32 +4,27 @@ session_start();
 require_once "../utils/formUtils.php";
 require_once "../utils/crudStories.php";
 
-
+$pageTitle = "View Stories";
 $crud = new CRUD_STORY();
-$stories = $crud->selectStories("");
+$stories = $crud->selectStories("");    
 $layout = "";
-
     foreach ($stories as $story) {  
-            $image = "../images/users/{$story['image']}";
+            $image = "../images/pets/{$story['image']}";
             $desc = $story["desc"];
             $layout .= <<<HTML
                 <div class='col-lg-3 col-md-4 col-sm-6'>
                     <div class='card'>
-                        <img src="<?php echo $image; ?>" alt="User-Photo" class="rounded-circle img-fluid" id="profile-picture">
+                        <img src="$image" alt="Pet-Photo" class="rounded-circle img-fluid" id="profile-picture">
                         <div class='card-body'>
-                            <p class='card-text'>{$desc}</p>
+                            <p class='card-text'>{$desc}</p>    
                         </div>
                     </div>
                 </div>
-        
    HTML;
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <?php include '../components/head.php'; ?>
     <link rel="stylesheet" href="../css/main.css">
